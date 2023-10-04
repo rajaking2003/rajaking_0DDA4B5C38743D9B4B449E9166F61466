98ffe617f0a8861d4ec2bef9cfea5f074dff857f
rajaking2003 <144811164+rajaking2003@users.noreply.github.com>
2023-10-04 12:06:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name =name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+      
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
 
 
-number = 2
-res = fact_rec(number)
-print("the factorial of {}is{}.".format(number, res))
+    return sorted_students
+
+students = [
+    Student("Hari","A123", 7.8),
+    Student("srikanth","A124", 8.9),
+    Student("Saumya","A125", 9.1),
+    Student("Mahidhar","A126", 9.9),
+    ]            
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+    print("Name:{}, Roll Number:{}, CGPA:{}".format(student.name,student.roll_number, student.cgpa))
